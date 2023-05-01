2b3443454443bfa8d95eb261b2c85c9dc4a56ce0
splimterXrodina <merah.soehyb@gmail.com>
2023-05-01 13:03:13 +0100

    fixing beforeunload and edit

--- src/routes/management/content/[space_name]/index.svelte
+++ src/routes/management/content/[space_name]/index.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
- import { params } from "@roxi/routify";
- import SpaceRenderer from "@/components/management/SpaceRenderer.svelte";
- import spaces from "@/stores/management/spaces";
+  import { params } from "@roxi/routify";
+  import SpaceRenderer from "@/components/management/SpaceRenderer.svelte";
+  import spaces from "@/stores/management/spaces";
 </script>
 
-{#if $params.space_name && spaces.is_loaded()}
-   <SpaceRenderer space_name={$params.space_name}/>
-  <!--pre>{JSON.stringify(spaces.get($params.space_name), null, 2)}</pre-->
-{:else}
-  <h4> For some reason ... params doesn't have the needed info</h4>
-  <pre>{JSON.stringify($params, null, 2)}</pre>
-{/if}
+{#key $params.space_name}
+  {#if $params.space_name && spaces.is_loaded()}
+    <SpaceRenderer bind:space_name={$params.space_name} />
+    <!--pre>{JSON.stringify(spaces.get($params.space_name), null, 2)}</pre-->
+  {:else}
+    <h4>For some reason ... params doesn't have the needed info</h4>
+    <pre>{JSON.stringify($params, null, 2)}</pre>
+  {/if}
+{/key}

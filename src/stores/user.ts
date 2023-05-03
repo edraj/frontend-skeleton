1933526cc4fa9f2627909f8b0a596d7d20e3756f
splimterXrodina <merah.soehyb@gmail.com>
2023-05-03 09:08:19 +0100

    fix issue with users with no displayname

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,11 +1,12 @@
-import { getLocaleFromNavigator } from "svelte-i18n";
+import { Writable, writable } from "svelte/store";
 import { login, logout } from "@/dmart";
-import { writable, Writable } from "svelte/store";
+
+import { getLocaleFromNavigator } from "svelte-i18n";
 
 enum Locale {
-  ar="ar",
-  en="en",
-};
+  ar = "ar",
+  en = "en",
+}
 
 export interface User {
   signedin: boolean;
@@ -15,33 +16,42 @@ export interface User {
   account?: Object;
 }
 
-const KEY='user';
+const KEY = "user";
 
 const fallback_locale = Locale.ar;
-function guess_locale() : Locale {
+function guess_locale(): Locale {
   const _locale = getLocaleFromNavigator();
 
-  if(_locale && _locale in Locale) {
+  if (_locale && _locale in Locale) {
     return Locale[_locale];
   }
 
   return fallback_locale;
 }
 
-let signedout: User = {signedin: false, locale: guess_locale()};
-export let user:Writable<User>;
+let signedout: User = { signedin: false, locale: guess_locale() };
+export let user: Writable<User>;
 
 // Load the user information from store, if it exists
-const data = (typeof localStorage !== 'undefined') ? (localStorage.getItem(KEY) || JSON.stringify(signedout)) : JSON.stringify(signedout);
+const data =
+  typeof localStorage !== "undefined"
+    ? localStorage.getItem(KEY) || JSON.stringify(signedout)
+    : JSON.stringify(signedout);
 user = writable<User>(JSON.parse(data) || signedout);
 
 export async function signin(username: string, password: string) {
   const response = await login(username, password);
   if (response.status == "success" && response.records.length > 0) {
     const account = response.records[0];
-    const _user: User = {signedin: true, locale: Locale.ar, shortname: account.shortname, localized_displayname: account.attributes.displayname.en, account: account};
+    const _user: User = {
+      signedin: true,
+      locale: Locale.ar,
+      shortname: account.shortname,
+      localized_displayname: account.attributes?.displayname?.en,
+      account: account,
+    };
     user.set(_user);
-    localStorage.setItem(KEY, JSON.stringify(_user))
+    localStorage.setItem(KEY, JSON.stringify(_user));
   } else {
     user.set(signedout);
     localStorage.setItem(KEY, JSON.stringify(signedout));
@@ -56,8 +66,8 @@ export async function signout() {
   // console.log("Completed signout process");
 }
 
-export function switchLocale(locale : Locale) {
-  user.update(user => {
+export function switchLocale(locale: Locale) {
+  user.update((user) => {
     user.locale = locale;
     signedout.locale = locale; // remember the locale value in case we logout
     localStorage.setItem(KEY, JSON.stringify(user));

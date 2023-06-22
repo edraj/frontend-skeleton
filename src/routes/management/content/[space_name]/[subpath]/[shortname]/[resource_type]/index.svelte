f1647e62815e054329ba63553f696fe219b63d8d
splimterXrodina <merah.soehyb@gmail.com>
2023-06-22 08:44:29 +0100

    fix health check

--- src/routes/management/content/[space_name]/[subpath]/[shortname]/[resource_type]/index.svelte
+++ src/routes/management/content/[space_name]/[subpath]/[shortname]/[resource_type]/index.svelte
@@ -7,7 +7,7 @@
 </script>
 
 {#if $params.space_name && $params.subpath && $params.shortname}
-  {#await retrieve_entry(resource_type, $params.space_name, $params.subpath.replaceAll("-", "/"), $params.shortname, true, true, $params.validate_schema !== "false")}
+  {#await retrieve_entry(resource_type, $params.space_name, $params.subpath.replaceAll("-", "/"), $params.shortname, true, true, $params.validate_schema === "false" ? false : true)}
     <!--h6 transition:fade >Loading ... @{$params.space_name}/{$params.subpath}</h6-->
   {:then entry}
     <EntryRenderer
